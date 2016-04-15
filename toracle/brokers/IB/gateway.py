58e3433d94a0a89154ca2a1516ba2c77c8948042
Jesse Liu <jesse@liu.onl>
2016-04-14 18:02:22 -0700

    Add IBWrapper and IBClient classes.

---
+++ toracle/brokers/IB/gateway.py
@@ -0,0 +1,112 @@
+"""
+Interactive Brokers API wrapper.
+"""
+
+import time
+from configparser import ConfigParser
+from swigibpy import EWrapper
+from swigibpy import EPosixClientSocket
+
+# Max wait time
+MAX_WAIT = 30
+
+
+def read_gateway_config(configfile='gwconfig.ini'):
+    """
+    :param
+             configfile: Configuration file containing IB Gateway IP, port.
+    :return:
+             IB Gateway IP and port.
+    :note:
+             Client ID is not specified in config file, instead at IBClient
+             instantialization.
+    """
+
+    gwconfig = ConfigParser()
+    gwconfig.read(configfile)
+    gwdefault = gwconfig['DEFAULT']
+    return gwdefault['host'], int(gwdefault['port'])
+
+
+class IBWrapper(EWrapper):
+    """
+
+    EWrapper implementation, which are callbacks passed to IBClient.
+
+    """
+
+    def init_error(self):
+        setattr(self, "flag_iserror", False)
+        setattr(self, "error_msg", "")
+
+    def error(self, id, errorCode, errorString):
+        """
+        error handling, simple for now
+
+        Here are some typical IB errors
+        INFO: 2107, 2106
+        WARNING 326 - can't connect as already connected
+        CRITICAL: 502, 504 can't connect to TWS.
+            200 no security definition found
+            162 no trades
+
+        """
+
+        ## Any errors not on this list we just treat as information
+        ERRORS_TO_TRIGGER = [201, 103, 502, 504, 509, 200, 162, 420, 2105, 1100, 478, 201, 399]
+
+        if errorCode in ERRORS_TO_TRIGGER:
+            errormsg = "IB error id %d errorcode %d string %s" % (id, errorCode, errorString)
+            print(errormsg)
+            setattr(self, "flag_iserror", True)
+            setattr(self, "error_msg", True)
+
+    ## time handling
+    def init_time(self):
+        setattr(self, "data_time_now", None)
+
+    def currentTime(self, time_from_server):
+        setattr(self, "data_time_now", time_from_server)
+
+    def nextValidId(self, orderId):
+        pass
+
+    def managedAccounts(self, openOrderEnd):
+        pass
+
+
+class IBClient(object):
+    def __init__(self, callback, clientid):
+        eclient = EPosixClientSocket(callback)
+        host, port = read_gateway_config()
+        eclient.eConnect(host, port, clientid)
+
+        self.eclient = eclient
+        self.cb = callback
+
+    def speaking_clock(self):
+        print("Getting the time... ")
+
+        self.eclient.reqCurrentTime()
+
+        start_time = time.time()
+
+        self.cb.init_error()
+        self.cb.init_time()
+
+        iserror = False
+        finished = False
+
+        while not (finished or iserror):
+            finished = self.cb.data_time_now is not None
+            iserror = self.cb.flag_iserror
+
+            if (time.time() - start_time) > MAX_WAIT:
+                finished = True
+
+            if iserror:
+                print("Error happened")
+                print(self.cb.error_msg)
+                finished = True
+
+        return self.cb.data_time_now
---
+++ toracle/brokers/IB/testib.py
@@ -0,0 +1,24 @@
+"""
+IB modules test scripts.
+"""
+
+from IB.gateway import IBWrapper, IBClient
+from swigibpy import Contract as IBContract
+
+
+if __name__ == "__main__":
+    IB_wrapper = IBWrapper()
+    IB_client_test = IBClient(IB_wrapper, 99)
+
+    # test 1
+    print(IB_client_test.speaking_clock())
+
+    # test 2
+    IB_contract = IBContract()
+    IB_contract.secType = 'FUT'
+    IB_contract.expiry = "201609"
+    IB_contract.symbol = "GE"
+    IB_contract.exchange = "GLOBEX"
+
+    # close connection
+    IB_client_test.eclient.eDisconnect()
