a5fc12e34137a9d6b3bb171c6ee38f8767fbc6a7
Jesse Liu <jesse@liu.onl>
2016-07-07 15:31:01 -0700

    Update IB MarketData interface.

--- toracle/brokers/IB/marketdata.py
+++ toracle/brokers/IB/marketdata.py
@@ -6,26 +6,19 @@ Obtaining, storing and processing historical and real-time market data.
 import pandas as pd
 
 
-# Globals holding data
-# All historical data stored on disk
-# hist_pool to be implemented with a database/storage (NoSql, HDF5)
-# Historical data cache stored in memory for current session
-# hist_cache = pd.DataFrame()
-
-
-class HistData(object):
+class MarketData(object):
     """
-    Historical market data cache.
+    Market data class.
     Orgainzed based on Pandas dataframe.
 
-    self.pool stores all the historical data in Pandas DataFrame format,
-    and its has no meaningful index (just integers by default).
+    self._depot stores all the historical data in Pandas DataFrame format,
+    with a pre-defined MultiIndex ['Ticker', 'DateTime',].
     """
 
-    def __init__(self, init_data=None):
+    def __init__(self, data=None):
         """
         Costruct HistData object and initialize data pool.
-        init_data: a list of named tuples, whose field_names are data names
+        data: a list of named tuples, whose field_names are data names
                    (ticker, date, price, etc.), which then merge to DataFrame
                    column names.
             Example:
@@ -37,42 +30,61 @@ class HistData(object):
         If init_data is empty(None), pool is initialized as an empty DataFrame.
         """
 
-        global hist_cache
-        if init_data is None:
-            self.pool = pd.DataFrame()
+        if data is None:
+            self._depot = pd.DataFrame()
         else:
             try:
-                self.pool = pd.DataFrame(init_data,
-                                         columns=init_data[0]._fields)
+                self._depot = pd.DataFrame(data, columns=data[0]._fields)
             except AttributeError:
                 print('HistData must be initialized either by empty data,\
                 or by a list of named tuples!')
 
-    def data_by_index(self, index_list):
+    @property
+    def depot(self, index_list=['ticker', 'date_time']):
         """
+        Implement data storage as a property.
         Return a copy of data pool with index set to the specified list.
+        Keyword Arguments:
         """
-        pool_copy = self.pool.copy()
+        # Filter out all valid index names in the specified list.
+        indices = [x for x in index_list if x in self._depot.columns]
 
-        # filter out the data names that do not exist.
-        exist_indices = [x for x in index_list if x in pool_copy.columns]
-        # Set indices
-        pool_copy.set_index(exist_indices, inplace=True)
-        return pool_copy
+        # If any valid index except "ticker" and "date_time" specified,
+        # make a copy of data frame, set its index, and return the copy.
+        if indices:
+            depot_copy = self._depot.copy()
+            depot_copy.set_index(indices, inplace=True)
+            return depot_copy
+        else:
+            return self._depot
 
     def add_data(self, data):
         """
-        Keyword Arguments:
-        data: list of named tuples.
         New data is merged with self.pool in the SQL "outer" (union) style.
         Note 1: Merging remove duplicate data rows.
-        Note 2: The index of self.pool is a series of integers, which has
+        Note 2: The index of self._depot is a series of integers, which has
         no actual meaning (not time series) & may be changed by merging.
-        """
-        try:
-            df_data = pd.DataFrame(data, columns=data[0]._fields)
-        except AttributeError:
-            print('HistData must be initialized either by empty data,\
-            or by a list of named tuples!')
 
-        self.pool.merge(df_data, how='outer')
+        Keyword Arguments:
+        data: market data in DataFrame format.
+        """
+        # Re-index new data to conform with depot format
+
+        # Merge with depot in SQL "outer"(union) style. Remove duplicate rows.
+        self._depot.merge(data, how='outer')
+
+
+# import pandas as pd
+# from collections import namedtuple
+
+
+# HistData = namedtuple('Price', 'Ticker DateTime Price')
+# a = Price('GE', pd.to_datetime('2010-01-11').tz_localize('US/Pacific'), 35.00)
+# b = Price('GE', pd.to_datetime('2010-01-12').tz_localize('US/Pacific'), 37.00)
+# l = [a, b]
+# df5 = pd.DataFrame(l, columns=l[0]._fields)
+
+
+# idx=pd.MultiIndex(levels=[[],[]],labels=[[],[]],names=['symbol','date'])
+
+# check if df6.index.names==idx.names
