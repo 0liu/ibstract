7dd0b139541ba1a529890bfb3a6daa7049f6f3f6
Jesse Liu <jesse@liu.onl>
2016-08-14 15:36:43 -0700

    First cut of IB historical data.

--- toracle/brokers/IBgateway.py
+++ toracle/brokers/IBgateway.py
@@ -4,18 +4,19 @@ Interactive Brokers API wrapper.
 
 import time
 import datetime
+import re
 import random
 from configparser import ConfigParser
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 
 from swigibpy import EWrapper, EPosixClientSocket
 from swigibpy import Contract as IBContract
 
-from marketdata import MarketData
+from marketdata import timedur_to_std
 
 
 # Max wait time
-MAX_WAIT = 30
+MAX_WAIT = 5
 
 
 def read_gateway_config(configfile='IBconfig.ini'):
@@ -35,15 +36,67 @@ def read_gateway_config(configfile='IBconfig.ini'):
     return gwdefault['host'], int(gwdefault['port'])
 
 
+def barsize_to_IB(barsize):
+    """Convert bar size string to IB style and check validility.
+
+    :param barsize: bar size in market data.
+    :returns: IB-style bar size string.
+    :rtype: string
+
+    """
+    bs = timedur_to_std(barsize)
+    IB_barsize_map = {
+        '1s': '1 secs',
+        '5s': '5 secs',
+        '10s': '10 secs',
+        '15s': '15 secs',
+        '30s': '30 secs',
+        '1m': '1 min',
+        '2m': '2 mins',
+        '3m': '3 mins',
+        '5m': '5 mins',
+        '10m': '10 mins',
+        '15m': '15 mins',
+        '20m': '20 mins',
+        '30m': '30 mins',
+        '1h': '1 hour',
+        '2h': '2 hours',
+        '3h': '3 hours',
+        '4h': '4 hours',
+        '8h': '8 hours',
+        '1d': '1 day',
+        '1W': '1W',
+        '1M': '1M'
+    }
+
+    try:
+        bs_IB = IB_barsize_map[bs]
+    except KeyError:
+        print("Error, barsize_to_IB(): Invalid input barsize string!")
+
+    return bs_IB
+
+
+def timedur_to_IB(time_dur_str):
+    tdur = timedur_to_std(time_dur_str)
+    tdur_num = re.findall('\d+', tdur)[0]
+    tdur_unit = tdur[-1]
+    if tdur_unit in ['s', 'd', 'W', 'M', 'Y']:
+        return tdur_num + ' ' + tdur_unit.upper()
+    else:
+        raise Exception(("Error, timedur_to_IB():"
+                         "Invalid time duration string for IB: {} !").
+                        format(time_dur_str))
+
 class IBWrapper(EWrapper):
     """
 
     EWrapper implementation, which are callbacks passed to IBClient.
 
     """
-    # def __init__(self):
-    #     super().__init__()
-    #     self.init_error()
+    def __init__(self):
+        super().__init__()
+        self.init_error()
 
     def error(self, errorid, errorCode, errorString):
         """
@@ -78,25 +131,23 @@ class IBWrapper(EWrapper):
     def init_time(self):
         self.date_time_now = None
 
-    def init_hist_data(self, HistDataTuple):
+    def init_hist_data(self):
         self.req_hist_data_done = False
-        self.hist_data_buf = MarketData()
-        self.HistDataTuple = HistDataTuple
+        self.hist_data_buf = []
 
     # ##########################################################
     # Following virtual functions are defined/declared in IB_API
 
     def currentTime(self, time_from_server):
-        self.data_time_now = time_from_server
+        self.date_time_now = time_from_server
 
     def historicalData(self, reqId, date, open_price, high, low, close,
                        volume, barCount, WAP, hasGaps):
         if date[:8] == 'finished':
             self.req_hist_data_done = True
         else:
-            self.hist_data_buf.update(
-                self.HistDataTuple(date, open_price, high, low, close, volume,
-                                   barCount, WAP, hasGaps))
+            self.hist_data_buf.append((date, open_price, high, low, close,
+                                       volume, barCount, WAP, hasGaps))
 
     def nextValidId(self, orderId):
         pass
@@ -138,7 +189,7 @@ class IBClient(object):
         finished = False
 
         while not (finished or iserror):
-            finished = self.cb.data_time_now is not None
+            finished = self.cb.date_time_now is not None
             iserror = self.cb.flag_iserror
 
             if (time.time() - start_time) > MAX_WAIT:
@@ -148,7 +199,7 @@ class IBClient(object):
                 print("Error happened")
                 print(self.cb.error_msg)
 
-        return self.cb.data_time_now
+        return self.cb.date_time_now
 
     def make_contract(self, security_type, symbol,
                       put_call='CALL', strike=0.0,
@@ -179,7 +230,7 @@ class IBClient(object):
         contract.right = put_call
         contract.expiry = expiry
         contract.multiplier = multiplier
-        contract.conId = random.randint(1001, 2000)
+        # contract.conId = random.randint(1001, 2000)
         return contract
 
     def req_hist_data(self, req_contract, req_endtime, req_len='1 w',
@@ -192,7 +243,6 @@ class IBClient(object):
                             ttt, opt. time zone: GMT,EST,PST,MST,AST,JST,AET.
         :param req_len: durationStr in IB API. Time duration of data.
         :param req_barsize: barSizeSetting in IB API. String. Time resolution.
-                            1/5/15/30 secs, 1/2/3/5/15/30 mins, 1 hour, 1 day.
         :param req_datatype: whatToShow in IB API. Requested data type.
                              "TRADES","MIDPOINT","BID", "ASK","BID_ASK",
                              "HISTORICAL_VOLATILITY",
@@ -205,25 +255,25 @@ class IBClient(object):
         """
 
         self.cb.init_error()
-
-        # Initalize data buffer and a namedtuple type for receiving data
-        if req_datatype.upper() == 'TRADES':
-            HistDataTuple = namedtuple('HistDataTuple',
-                                       'DateTime, Open, High, Low, Close,'
-                                       'Volume, BarCount, WAP, HasGaps')
-        else:
-            HistDataTuple = namedtuple('HistDataTuple',
-                                       'DateTime, Open, High, Low, Close,'
-                                       'Volume, BarCount, WAP, HasGaps')
-        self.cb.init_hist_data(HistDataTuple)
+        self.cb.init_hist_data()
 
         # Generate a random request Id in the range of [100,1000]
         req_id = random.randint(2000, 3000)
 
+        # Convert input strings to IB style
+        IB_barsize = barsize_to_IB(req_barsize)
+        IB_duration = timedur_to_IB(req_len)
+        IB_whattoshow = req_datatype.upper()
+        if IB_whattoshow not in ("TRADES", "MIDPOINT", "BID", "ASK", "BID_ASK",
+                                 "HISTORICAL_VOLATILITY",
+                                 "OPTION_IMPLIED_VOLATILITY"):
+            raise Exception("Invalid requested IB data type: {} !".
+                            format(req_datatype))
+
         # call EClientSocket function to request historical data
         self.ec.reqHistoricalData(req_id, req_contract, req_endtime,
-                                       req_len, req_barsize, req_datatype,
-                                       useRTH, 1, None)
+                                  IB_duration, IB_barsize, IB_whattoshow,
+                                  useRTH, 1, None)
 
         # Loop to check if request finished
         start_time = time.time()
@@ -235,18 +285,26 @@ class IBClient(object):
             if(time.time() - start_time) > MAX_WAIT:
                 iserror = True
             pass
-
         if iserror:
             print(self.cb.error_msg)
-            raise Exception("Error requesting historic data.")
+            raise Exception("Error requesting historic data!")
 
-        return self.cb.hist_data
+        # Add symbol and BarSize, and convert data to a list of namedtuples
+        DataRow = namedtuple('HistDataTuple',
+                             'Symbol, BarSize,'
+                             'DateTime, Open, High, Low, Close,'
+                             'Volume, BarCount, WAP, HasGaps')
+        hist_data = []
+        for data in self.cb.hist_data_buf:
+            hist_data.append(DataRow(
+                *((req_contract.symbol, req_barsize) + data)))
+
+        return hist_data
 
 
 if __name__ == "__main__":
-    import pdb
     import sys
-    # pdb.set_trace()
+    from marketdata import MarketData
     IB_wrapper = IBWrapper()
     IB_client_test = IBClient(IB_wrapper)
 
@@ -258,16 +316,17 @@ if __name__ == "__main__":
     if (not sys.argv[1:]) or (int(sys.argv[1]) == 2):
         security_type = 'STK'
         symbol = 'FB'
-        req_endtime = '20160812 16:00:00 EST'
-        req_len = '1 w'
-        req_barsize = '1 day'
+        req_endtime = '20150817 11:35:00'
+        req_len = '100 s'
+        req_barsize = '5 sec'
         req_datatype = 'TRADES'
 
         req_contract = IB_client_test.make_contract(
             security_type, symbol)
-        hist_data = IB_client_test.req_hist_data(
+        hist_data_list = IB_client_test.req_hist_data(
             req_contract, req_endtime, req_len, req_barsize, req_datatype)
-        print(hist_data)
+        hist_data = MarketData(hist_data_list)
+        print(hist_data.depot)
 
     # close connection
     IB_client_test.ec.eDisconnect()
--- toracle/brokers/marketdata.py
+++ toracle/brokers/marketdata.py
@@ -6,6 +6,7 @@ historical/real-time market data.
 """
 
 
+import re
 import pandas as pd
 from collections import namedtuple
 
@@ -28,6 +29,58 @@ def is_list_namedtuple(l):
         and all(is_namedtuple(x) for x in l)
 
 
+def timedur_to_std(time_dur_str):
+    """Convert bar size to standard abbreviations:
+       1. No space.
+       2. One letter represents unit.
+       3. s,m,h,d,W,M for seconds, minutes, hours, days, weeks and months.
+
+    :param time_dur_str: A time duration string, like '1 min', '5days',etc.
+    :returns: standardized bar size.
+    :rtype: string.
+
+    """
+
+    # find all digits
+    bs_num = re.findall('\d+', time_dur_str)[0]
+
+    # find all letters
+    bs_strs = re.findall('[a-zA-Z]', time_dur_str)
+
+    if len(bs_strs) == 1:
+        # If only one letter, lower/upper case "m"/"M" to diff min and month
+        bs_unit = bs_strs[0].lower()
+        if bs_unit not in ('s', 'm', 'h', 'd', 'w', 'y'):
+            raise Exception('Invalid bar size unit: {}!'.format(time_dur_str))
+        if bs_strs[0] in ('w', 'W', 'M'):  # Upper case for week/month
+            bs_unit = bs_unit.upper()
+    else:
+        unit_map = {
+            'sec': 's',
+            'min': 'm',
+            'hour': 'h',
+            'hr': 'h',
+            'day': 'd',
+            'wk': 'W',
+            'week': 'W',
+            'mo': 'M',
+            'mon': 'M',
+            'yr': 'Y',
+            'year': 'Y'
+        }
+
+        bs_unit = ''
+        for k in unit_map.keys():
+            bs_strs = re.findall(k, time_dur_str)
+            if bs_strs:
+                bs_unit = unit_map[k]
+                break
+        if not bs_unit:
+            raise Exception("Invalid bar size unit: {}".format(time_dur_str))
+
+    return bs_num + bs_unit
+
+
 class MarketData(object):
     """
     Market data cache, for either historical or real-time data.
@@ -38,9 +91,9 @@ class MarketData(object):
 
     self._depot stores data in pandas DataFrame format, with pre-defined
     MultiIndex format:
-        ['Ticker', 'Interval', 'DateTime']
-        'Ticker': string.
-        'Interval': pandas Timedelta.
+        ['Symbol', 'BarSize', 'DateTime']
+        'Symbol': string.
+        'BarSize': pandas Timedelta.
         'DateTime': pandas DatetimeIndex.
     """
 
@@ -51,7 +104,7 @@ class MarketData(object):
         """
 
         # Pre-defined depot index.
-        self.depot_index = ['Ticker', 'Interval', 'DateTime']
+        self.depot_index = ['Symbol', 'BarSize', 'DateTime']
 
         # Intialize depot.
         self._depot = pd.DataFrame()
@@ -64,7 +117,7 @@ class MarketData(object):
         Implement data storage as a property.
         Return a copy of data depot with index set to the specified list.
 
-        :param index: List of wanted index. E.g. ['Ticker','DateTime']
+        :param index: List of wanted index. E.g. ['Symbol','DateTime']
 
         """
         # Filter out all valid index names in the specified list.
@@ -82,9 +135,9 @@ class MarketData(object):
         New data is combined with self._depot and overwritten with non-null
         values of input data. Indexes and Columns will be unioned.
 
-        :param data: List of namedtuples, or dataframe. Market data slices.
-
+        :param data: List of namedtuples, or dataframe, with market data rows.
         """
+
         # Check input data type
         if not((isinstance(data, pd.DataFrame)) or is_list_namedtuple(data)):
             print('''Input data must be a dataframe or list of namedtuples.
@@ -98,18 +151,18 @@ class MarketData(object):
 
         # Reset dataframe index, or convert namedtuples to dataframe.
         if isinstance(data, pd.DataFrame):
-            data.reset_index(inplace=True)
+            data_df = data.reset_index()
         else:  # data is a list of namedtuple
             data_df = pd.DataFrame(data, columns=data[0]._fields)
 
         # Now set index to pre-defined, convert time index, and combine values.
         try:
-            data_df['Interval'] = pd.TimedeltaIndex(data_df['Interval'])
+            data_df['BarSize'] = data_df['BarSize'].map(timedur_to_std)
             data_df['DateTime'] = pd.DatetimeIndex(data_df['DateTime'])
-            data.set_index(self.depot_index, inplace=True)
+            data_df.set_index(self.depot_index, inplace=True)
         except KeyError:
             print('''Input data fields must comply with pre-defined index:
-            ['Ticker','Interval','DateTime']
+            ['Symbol','BarSize','DateTime']
             Input data is not combined. Abort!''')
             return
         if self._depot.empty:  # initializing depot
@@ -117,71 +170,43 @@ class MarketData(object):
         else:
             self._depot = data_df.combine_first(self._depot)
 
+    def combine(self, another_market_data):
+        """Combine the data in depot with another MarketData object.
 
-def main():
-    """Unit test main function.
-    :returns: None
-    :rtype: None
-    """
+        :param another_market_data: Another MarketData() object.
+        :returns: No return. Side effect: self._depot is updated.
+        """
+        if another_market_data is MarketData:
+            self.update(another_market_data.depot)
+        else:
+            print("Input object is not a MarketData class object. Abort!")
 
-    # depot_index = pd.MultiIndex(levels=[[], [], []],
-    #                             labels=[[], [], []],
-    #                             names=['Ticker', 'Interval', 'DateTime'])
-    # depot_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
-
-    DataItem1 = namedtuple('DataItem', 'Ticker Interval DateTime Close')
-    # intvl = pd.Timedelta('5 min')
-    mkt_data1 = [
-        DataItem1('GS', '5 min', '2016-07-12 10:35:00', 140.05),
-        DataItem1('GS', '5 min', '2016-07-12 11:20:00', 141.34),
-    ]
-    DataItem = namedtuple('DataItem', 'Ticker Interval DateTime Close Volume')
-    mkt_data = [
-        DataItem('FB', '5 min', '2016-07-21 09:30:00', 120.05, 234242),
-        DataItem('FB', '5 min', '2016-07-21 09:35:00', 120.32, 410842),
-        DataItem('FB', '1 min', '2016-07-25 09:40:00', 120.47, 579638),
-        DataItem('FB', '1 min', '2016-07-25 09:41:00', 120.82, 192476),
-        DataItem('AMZN', '1 day', '2016-07-21', 749.22, 27917),
-        DataItem('AMZN', '1 day', '2016-07-22', 738.87, 36662),
-        DataItem('AMZN', '1 day', '2016-07-23', 727.23, 8766),
-    ]
-    mkt_data2 = [
-        DataItem('GS', '5 min', '2016-07-12 10:35:00', 140.05, 344428),
-    ]
-
-    global mkt_data1_df
-    mkt_data1_df = pd.DataFrame(mkt_data1, columns=mkt_data1[0]._fields)
-    mkt_data1_df['Interval'] = pd.TimedeltaIndex(mkt_data1_df['Interval'])
-    mkt_data1_df['DateTime'] = pd.DatetimeIndex(mkt_data1_df['DateTime'])
-    print(mkt_data1_df)
-
-    global mkt_data2_df
-    mkt_data2_df = pd.DataFrame(mkt_data2, columns=mkt_data2[0]._fields)
-    mkt_data2_df['Interval'] = pd.TimedeltaIndex(mkt_data2_df['Interval'])
-    mkt_data2_df['DateTime'] = pd.DatetimeIndex(mkt_data2_df['DateTime'])
-    print(mkt_data2_df)
-
-    global mkt_data_df
-    mkt_data_df = pd.DataFrame(mkt_data, columns=mkt_data[0]._fields)
-    mkt_data_df['Interval'] = pd.TimedeltaIndex(mkt_data_df['Interval'])
-    mkt_data_df['DateTime'] = pd.DatetimeIndex(mkt_data_df['DateTime'])
-    mkt_data_df.set_index(MarketData.depot_index)
-    print(mkt_data_df)
-
-    global depot
-    depot = pd.DataFrame()
-
-    if depot.empty:
-        depot = mkt_data1_df.set_index(MarketData.depot_index)
-        depot.sortlevel(inplace=True)
-    print('\n\n', depot)
-
-    # merge
-    col_inter = list(depot.columns.intersection(mkt_data_df.columns))
-    depot = pd.merge(depot, mkt_data_df, how='outer', left_index=True,
-                     right_index=True, on=col_inter)
-    # depot.sortlevel(inplace=True)
-    # print('\n\n\n', depot)
 
 if __name__ == '__main__':
-    main()
+    DataRow_simple = namedtuple('DataRow_simple',
+                                'Symbol BarSize DateTime Close')
+    DataRow = namedtuple('DataRow', 'Symbol BarSize DateTime Close Volume')
+
+    mkt_data_simple = [
+        DataRow_simple('GS', '5 min', '2016-07-12 10:35:00', 140.05),
+        DataRow_simple('GS', '5 min', '2016-07-12 11:20:00', 141.34),
+    ]
+
+    mkt_data_full = [
+        DataRow('FB', '5 min', '2016-07-21 09:30:00', 120.05, 234242),
+        DataRow('FB', '5 min', '2016-07-21 09:35:00', 120.32, 410842),
+        DataRow('FB', '1 min', '2016-07-25 09:40:00', 120.47, 579638),
+        DataRow('FB', '1 min', '2016-07-25 09:41:00', 120.82, 192476),
+        DataRow('AMZN', '1 day', '2016-07-21', 749.22, 27917),
+        DataRow('AMZN', '1 day', '2016-07-22', 738.87, 36662),
+        DataRow('AMZN', '1 day', '2016-07-23', 727.23, 8766),
+    ]
+
+    mkt_data_oneline = [
+        DataRow('GS', '5 min', '2016-07-12 10:35:00', 140.05, 344428),
+    ]
+
+    mktdata = MarketData(mkt_data_simple)
+    mktdata.update(mkt_data_oneline)
+    mktdata.update(mkt_data_full)
+    print(mktdata.depot)
